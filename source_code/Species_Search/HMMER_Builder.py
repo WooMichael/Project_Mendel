adf8ce2287c82a418e724d4bacb5b903b91e8392
Ubuntu <ubuntu@ip-172-31-47-1.us-east-2.compute.internal>
2020-07-03 20:56:31 +0000

    built HMMs for Species and Genera

--- source_code/Species_Search/HMMER_Builder.py
+++ source_code/Species_Search/HMMER_Builder.py
@@ -1,23 +1,22 @@
 import os
 from Bio import SeqIO
 
-stream = os.popen(
-    'ls home/ubuntu/Github/Project_Mendel/Data/Genera_Search_Data/Aligned_Reference_Sequences_From_LTP_Species/')
+stream = os.popen('ls /home/ubuntu/Github/Project_Mendel/Data/Species_Search_Data/Aligned_Reference_Sequences_From_LTP_Species/')
 output = stream.readlines()
-ref_path = "home/ubuntu/Github/Project_Mendel/Data/Genera_Search_Data/Aligned_Reference_Sequences_From_LTP_Species/"
-hmm_path = "home/ubuntu/Github/Project_Mendel/Data/Genera_Search_Data/HMM_Builds_From_Aligned_Sequences_LTP_Species/"
+ref_path = "/home/ubuntu/Github/Project_Mendel/Data/Species_Search_Data/Aligned_Reference_Sequences_From_LTP_Species/"
+hmm_path = "/home/ubuntu/Github/Project_Mendel/Data/Species_Search_Data/HMM_Builds_From_Aligned_Sequences_LTP_Species/"
 
 for fam in output:
-    family = str(fam).strip()
+    family = str(fam).rstrip('\n')
     path = ref_path + family
     gen_stream = os.popen("ls " + path)
     gen_output = gen_stream.readlines()
-    dir_gen_path = hmm_path + fam + "/"
+    dir_gen_path = hmm_path + family + "/"
     if not os.path.exists(dir_gen_path):
         os.mkdir(dir_gen_path)
     for gen in gen_output:
         hmm_gen = gen.replace('.fa', '.hmm')
         gen_path = path + "/" + gen
-        command_line_msa = "hmmbuild " + dir_gen_path + hmm_gen + " " + gen_path
+        command_line_msa = "hmmbuild " + dir_gen_path + hmm_gen.strip() + " " + gen_path
         print(command_line_msa)
         os.popen(str(command_line_msa))
